da2b75bc8d4455bc5930add1428bd131a00f4db7
Marc Nuri <marc@marcnuri.com>
2023-01-26 15:53:01 +0100

    feat(frontend): ready to be played with

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,13 @@
 import {api} from './orders/api';
-import './App.css';
 
 function App() {
   const {data: orders} = api.endpoints.getRecentOrders.useQuery(undefined, {pollingInterval: 10000});
   return (
-    <div className="App">
-      {(orders ?? []).map(o => <div key={o.orderId}>{o.orderId}</div>)}
+    <div className="react-fronted">
+      <h1>Latest Orders</h1>
+      {(orders ?? []).map(({orderId, customer: {companyName}}) =>
+        <div key={orderId} className='order'>{orderId} - {companyName}</div>
+      )}
     </div>
   );
 }
