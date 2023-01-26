963196a26bdbceee903073d2256bdb052b12fde9
Marc Nuri <marc@marcnuri.com>
2023-01-26 15:44:14 +0100

    feat(frontend): can access Northwind service (CORS)

--- react-frontend/index.js
+++ react-frontend/index.js
@@ -2,9 +2,17 @@
 /* eslint-disable no-console */
 
 const express = require('express');
+const url = require('url');
+const proxy = require('express-http-proxy');
+
 const app = express();
 
-app.use(express.static(`${__dirname}/build`)); // Serves resources from public folder
+app.use(express.static(`${__dirname}/build`));
+
+const apiProxy = proxy('http://northwind:8080', {
+  proxyReqPathResolver: req => req.originalUrl
+});
+app.use('/api/*', apiProxy);
 
 app.listen(8080);
-console.log('Server started, http://localhost:80/');
+console.log('Server started, http://localhost:8080/');
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {Provider} from 'react-redux';
 import App from './App';
+import {store} from './store'
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
